22f1820e7965490cd2a536e0ed9c1be1fb0faebc
ryno888 <ryno888@gmail.com>
2018-05-28 08:39:29 +0200

    OBSERVATION SHEET PDF Layout

--- src/fxschoolapp/document/modules/PdfObservationSheet.java
+++ src/fxschoolapp/document/modules/PdfObservationSheet.java
@@ -9,35 +9,17 @@
  */
 package fxschoolapp.document.modules;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Phrase;
-import com.itextpdf.text.html.simpleparser.HTMLWorker;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
-import com.itextpdf.tool.xml.html.Tags;
-import com.itextpdf.tool.xml.pipeline.css.CSSResolver;
-import com.itextpdf.tool.xml.pipeline.html.HtmlPipelineContext;
 import core.Core;
 import core.com.pdf.ComPdf;
 import static j2html.TagCreator.*;
 import j2html.tags.ContainerTag;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.StringReader;
 import java.net.URL;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -47,6 +29,7 @@ import java.util.logging.Logger;
  */
 public class PdfObservationSheet extends ComPdf{
     private String studentGrade = "Grade R";
+    private String studentBirthday = "23-12-1988";
     private String classYear = "2005";
     private String file;
     //--------------------------------------------------------------------------
@@ -73,18 +56,69 @@ public class PdfObservationSheet extends ComPdf{
                 style(getStyle())
             ),
             body(
+                //-----------------------------------------------------------------
                 table(
                     tr(
                         td(
-                            div("OBSERVATION SHEET").withClass("font12 text-center p-10")
-                        ).attr("colspan", 3).withClass("no-border-left")
-                    ).withClass("no-border-left"),
+                            div("OBSERVATION SHEET").withClass("font12 text-center p-5 font-weight-bold")
+                        ).attr("colspan", 12)
+                    ),
                     tr(
-                        td("NAME").withClass("w-80"),
-                        td("GRADE").withClass("w-10"),
-                        td("YEAR").withClass("w-10")
+                        td("NAME").withClass("font-weight-bold").attr("colspan", 6),
+                        td(p("GRADE").withClass("font-weight-bold"),p(studentGrade)).attr("colspan", 1),
+                        td(p("YEAR").withClass("font-weight-bold"),p(classYear)).attr("colspan", 1),
+                        td("").attr("colspan", 4)
+                    ),
+                    tr(
+                        td("BIRTH DATE").withClass("font-weight-bold").attr("colspan", 4),
+                        td(
+                            table(
+                                tr(
+                                    td("PREV. GRADE").withClass("font-weight-bold font10").attr("colspan", 4),
+                                    td("PP").withClass("font10 text-center").attr("colspan", 2),
+                                    td("R").withClass("font10 text-center").attr("colspan", 2),
+                                    td("1").withClass("font10 text-center").attr("colspan", 2),
+                                    td("2").withClass("font10 text-center").attr("colspan", 2),
+                                    td("3").withClass("font10 text-center").attr("colspan", 2)
+                                )
+                            ).withClass("w-100")
+                        ).attr("colspan", 4),
+                        td(div("PREVIOUS SCHOOL").withClass("fl font-weight-bold")).attr("colspan", 4)
+                    ),
+                    tr(
+                        td(studentBirthday).withClass("font-weight-bold").attr("colspan", 4),
+                        td(
+                            table(
+                                tr(
+                                    td("GRADE REPEATED").withClass("font-weight-bold font10").attr("colspan", 4),
+                                    td("1").withClass("font10 text-center").attr("colspan", 2),
+                                    td("2").withClass("font10 text-center").attr("colspan", 2),
+                                    td("3").withClass("font10 text-center").attr("colspan", 2)
+                                )
+                            ).withClass("w-100")
+                        ).attr("colspan", 4),
+                        td().attr("colspan", 4)
                     )
-                ).attr("cellspacing", 0).attr("cellpadding", 2).withClass("w-100")
+                ).attr("cellspacing", 0).withClass("main w-100"),
+                    
+                //-----------------------------------------------------------------
+                div().withClass("h-20"),
+                //-----------------------------------------------------------------
+                
+                table(
+                    tr(
+                        td(
+                            table(
+                                tr(
+                                    td("INTERVENTION hist.").withClass("font-weight-bold font10").attr("colspan", 3),
+                                    td("YR/GR").withClass("font10 text-center").attr("colspan", 1),
+                                    td("REMARK").withClass("font10 text-center").attr("colspan", 4)
+                                )
+                            ).withClass("w-100")
+                        ).attr("colspan", 8),
+                        td("Current Year/Remark").attr("colspan", 4)
+                    )
+                ).attr("cellspacing", 0).withClass("main w-100")
             )
         );
     }
